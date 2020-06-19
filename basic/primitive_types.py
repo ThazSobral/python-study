b36117c9a4b918b40fb567d6345a92c133feccfd
Thalles Sobral <thazsobral@gmail.com>
2020-06-19 14:39:08 -0300

    alteração na representação dos valores

--- basic/primitive_types.py
+++ basic/primitive_types.py
@@ -5,7 +5,7 @@ value_boolean = True
 value_string = 'Hello'
 
 #saída dos tipos de cada variável
-print(type(number_integer))
-print(type(number_float))
-print(type(value_boolean))
-print(type(value_string))
+print('O valor {} é {}.'.format(number_integer, type(number_integer)))
+print('O valor {} é {}.'.format(number_float, type(number_float)))
+print('O valor {} é {}.'.format(value_boolean, type(value_boolean)))
+print('O valor {} é {}.'.format(value_string, type(value_string)))
