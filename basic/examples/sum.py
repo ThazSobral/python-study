938e8b5790c08b7c0a64ec252b9d105c1250e80e
thazsobral <thazsobral@gmail.com>
2020-06-21 16:42:19 -0300

    adicionei a script de soma simples

---
+++ basic/examples/sum.py
@@ -0,0 +1,6 @@
+# soma de dois valores
+
+first_value = int(input('Digite o primeiro valor: '))
+second_value = int(input('Digite o segundo valor: '))
+
+print('A soma de {} e {} é {}'.format(first_value, second_value, (first_value + second_value)))
