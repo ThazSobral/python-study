e3f23a44ce62e40f2e99c4ce1e4071d2aa1ee0b1
thazsobral <thazsobral@gmail.com>
2020-06-21 16:41:46 -0300

    adicionei o script de apresentaçao de conteudo no prit

---
+++ basic/examples/birthday.py
@@ -0,0 +1,5 @@
+day = int(input('Em que dia você nasceu?'))
+month = input('Em que mês você nasceu?')
+year = int(input('Em que ano você nasceu?'))
+
+print('Você nasceu no dia {} de {} de {}, correto?'.format(day, month, year))
