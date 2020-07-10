a3bd33b7a1e06c8f462c2185db7583681b76f09c
Thalles Sobral <thazsobral@gmail.com>
2020-07-10 14:53:57 -0300

    adicionei o conversor

---
+++ basic/examples/conversor.py
@@ -0,0 +1,5 @@
+# Conversão de medida de metros para centímetros e milímetros.
+
+value = float(input('Digite o valor em metros: '))
+
+print('A medida de {}m em centímetros é {:.0f} e em milímetro é {:.0f}.'.format(value, value * 100, value * 1000))
