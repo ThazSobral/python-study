ae78b01f855c53278f92004e37532b53a5181400
Thalles Sobral <thazsobral@gmail.com>
2020-08-03 15:02:03 -0300

    adicionei novos scripts

---
+++ intermediary/examples/api-request.py
@@ -0,0 +1,15 @@
+# Esse script se destina a realizar uma requisição para uma api e mostrar na tela a resposta
+
+import urllib.request
+# utilizamos a biblioteca de json para interpretar a estrutura json
+import json
+
+# determinamos a url que vamos utilizar
+url = 'http://api.icndb.com/jokes/random/limitTo=[nerdy]'
+
+# realizamos a requisição
+response = urllib.request.urlopen(url).read()
+# lemos a resposta carregando uma estrutura em json com decodificação UTF-8
+data = json.loads(response.decode('utf8'))
+
+print(data['values']['joke'])
--- intermediary/terminal-with-python.py
+++
@@ -1,6 +0,0 @@
-# executar comandos de terminal utilizando o python pode economizar muito tempo para determinadas tarefas
-# para isso podemos utilizar a biblioteca OS
-import os
-
-# o método system permite digitar comandos puros de terminal e executá-los
-os.system('python -m pip install --user virtulenv')
